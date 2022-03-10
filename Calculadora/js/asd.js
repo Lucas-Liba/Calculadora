02304a5f9758c462f131a736933ff43740c015c1
Lucas-Liba <90228351+Lucas-Liba@users.noreply.github.com>
2022-03-10 14:27:56 -0300

    Add files via upload

---
+++ Calculadora/js/asd.js
@@ -0,0 +1,9 @@
+let x = 5;
+
+function suma(){
+x=x+1
+
+}
+
+suma()
+console.log(x)
---
+++ Calculadora/js/script.js
@@ -0,0 +1,89 @@
+const campo1 = document.querySelector("#campo1");
+
+const campo2 = document.querySelector("#campo2");
+
+let numero1 = null,
+  numero2;
+
+campo1.addEventListener("input", (e) => (numero1 = e.target.value));
+
+campo2.addEventListener("input", (e) => (numero2 = e.target.value));
+
+const botonSumar = document.querySelector("#sumar");
+const botonRestar = document.querySelector("#restar");
+const botonMultiplicar = document.querySelector("#multiplicar");
+const botonDividir = document.querySelector("#dividir");
+
+const respuesta = document.createElement("H1");
+document.body.appendChild(respuesta);
+
+botonSumar.addEventListener("click", () => {
+  if (numero1 == "" || numero2 == "") {
+    respuesta.textContent = "Alguno de los campos esta vacio";
+    return;
+  }
+
+  let resultado = parseInt(numero1) + parseInt(numero2);
+
+  respuesta.textContent = resultado;
+
+  reestablecerValores();
+});
+//resta
+
+botonRestar.addEventListener("click", () => {
+  if (numero1 == "" || numero2 == "") {
+    respuesta.textContent = "Alguno de los campos esta vacio";
+    return;
+  }
+
+  let resultado = parseInt(numero1) - parseInt(numero2);
+
+  respuesta.textContent = resultado;
+
+  reestablecerValores();
+});
+
+//multiplicacion
+
+botonMultiplicar.addEventListener("click", () => {
+  if (numero1 == "" || numero2 == "") {
+    respuesta.textContent = "Alguno de los campos esta vacio";
+    return;
+  }
+
+  let resultado = parseInt(numero1) * parseInt(numero2);
+
+  respuesta.textContent = resultado;
+
+  reestablecerValores();
+});
+
+//division
+
+botonDividir.addEventListener("click", () => {
+  if (numero1 == "" || numero2 == "") {
+    respuesta.textContent = "Alguno de los campos esta vacio";
+    return;
+  }
+
+  if (numero2 == "0") {
+    respuesta.textContent("No es posible dividir por 0");
+    return;
+  }
+
+  let resultado = parseInt(numero1) / parseInt(numero2);
+
+  respuesta.textContent = resultado;
+
+  reestablecerValores();
+});
+
+function reestablecerValores() {
+  campo1.value = "";
+
+  campo2.value = "";
+
+  numero1 = "";
+  numero2 = "";
+}
